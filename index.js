62680bac910cf9c31fff832dc329e6122d8ceccd
Justin Schow <Justin Schow>
2024-01-22 10:37:37 -0700

    Create database connection

--- index.js
+++ index.js
@@ -12,18 +12,12 @@ app.listen(port,()=>{
     console.log(`API is listening on port: ${port}`)
 }); // listen for web requests from the frontend and don't stop
 
-const boxes = [
-    {boxId:1},
-    {boxId:2},
-    {boxId:3},
-    {boxId:4}
-];
-
 // 1 - URL
 // 2 - function to return boxes
 // req = the request from the browser
 // res = the response to the browser
 app.get('/boxes',(req,res)=>{
+    let boxes = redisClient.json.get('boxes',{path:'$'}); // get the boxes
     // send the boxes to the browser
     res.send(JSON.stringify(boxes));
 });// return boxes to user
